cc9873e7e4c0f75eca4bdf613635806abdff1136
ModernShrub <75245177+ModernShrub@users.noreply.github.com>
2021-05-12 22:16:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -13,5 +13,5 @@ var firebaseConfig = {
 function addUser() {
     user_name = document.getElementById("user_name").value;
     localStorage.setItem("user_name", user_name);
-    window.location = "kwitter_room.htm";
+    window.location = "kwitter_room.html";
 }
